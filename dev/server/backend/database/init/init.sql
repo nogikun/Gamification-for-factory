64c8aacb4224bd1db11db4d1fe66329523455d0c
kanataiwamoto <luckykanata@gmail.com>
2025-04-24 16:04:25 +0900

    init.sqlファイルを編集しapplicantテーブルの作成

--- dev/server/backend/database/init/init.sql
+++ dev/server/backend/database/init/init.sql
@@ -1,11 +1,19 @@
--- テストテーブルの作成
-CREATE TABLE test (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+CREATE TABLE applicant (
+  user_id UUID PRIMARY KEY,
+  last_name VARCHAR(50) NOT NULL,
+  first_name VARCHAR(50) NOT NULL,
+  mail_address TEXT,
+  phone_number VARCHAR(50),
+  address TEXT,
+  birth_date TIMESTAMP,
+  license TEXT,
+  updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- テストデータの挿入
-INSERT INTO test (name, description) VALUES ('テスト項目1', 'これはテストデータです');
-INSERT INTO test (name, description) VALUES ('テスト項目2', '別のテストデータです');
+
+
+INSERT INTO applicant (
+    user_id, last_name, first_name, mail_address, phone_number, address, birth_date, license, updated_at 
+) VALUES (
+    'A1', '石田', '省吾', 'ishida@gmail.com', '012000000000', '大阪', '2000-03-21', '漢検5級', CURRENT_TIMESTAMP
+);
